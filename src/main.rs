a7d66f7990656d7b8da2641c313878806a146acd
sandreas <2050604+sandreas@users.noreply.github.com>
2025-11-22 12:42:09 +0100

    made play test sound work on desktop environment

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use std::io::Write;
-use tokio::sync::mpsc;
-use std::time::Duration;
 use clap::Parser;
 use std::fs::OpenOptions;
+use std::io::Write;
+use tokio::sync::mpsc;
 
 mod player;
 
@@ -15,17 +14,18 @@ struct Args {
     base_directory: String,
 }
 
+use crate::player::{Player, PlayerCommand, PlayerEvent};
+use cpal::traits::{DeviceTrait, HostTrait};
+use rodio::{OutputStreamBuilder, Sink};
 use slint::{
     ComponentHandle,
     Model,
     ModelRc,
     SharedString,
-    VecModel,
-    private_unstable_api::re_exports::euclid::num::{Round}
+    VecModel
 };
 use std::iter;
 use std::path::Path;
-use crate::player::{Player, PlayerCommand, PlayerEvent};
 
 slint::include_modules!();
 
@@ -37,17 +37,63 @@ async fn main() -> Result<(), slint::PlatformError> {
     let args = Args::parse();
     println!("{}", args.base_directory);
 
-    
+
+
+    let host = cpal::default_host();
+    let mut device = None;
+    for _ in 1..3 {
+        device = Some(host.output_devices().unwrap()
+            .find(|d| {
+                if let Ok(name) = d.name() {
+                    // sysdefault:CARD=A
+                    let match_string = "USB-C to 3.5mm Headphone Jack A";
+                    let match_string2 = "pipewire";
+                    // ALSA device names may include "hw:2,0" or "hw-2-0"
+                    // You might need to tune this filter depending on your device naming
+                    println!("device: {} contains {}: {}", name, match_string, name.contains(match_string));
+                    name.contains(match_string) || name.contains(match_string2)
+                } else {
+                    false
+                }
+            })).unwrap();
+
+
+        if device.is_some() {
+            break;
+        }
+    }
+
+    if !device.is_some() {
+        device = Some(host.default_output_device().unwrap());
+    }
+
+
+
+    let selected_device = device.unwrap();
+    /*
+    let default_config = selected_device.default_output_config().ok().unwrap();
+    let sample_rate = default_config.sample_rate().0;
+    let channel_count = default_config.channels();
+    let sample_format = default_config.sample_format();
+    */
+
+    let builder_result = OutputStreamBuilder::from_device(selected_device);
+    let builder = builder_result.unwrap();
+
+    // let builder = OutputStreamBuilder::from_default_device().unwrap();
+
+
+    let stream = builder.open_stream_or_fallback().unwrap();
+
+    let sink = Sink::connect_new(stream.mixer());
 
 
     let (cmd_tx, cmd_rx) = mpsc::unbounded_channel::<PlayerCommand>();
     let (evt_tx, mut evt_rx) = mpsc::unbounded_channel::<PlayerEvent>();
-    let worker = Player {
-        name: "player".to_string()
-    };
+    let player = Player::new("player".to_string(), sink);
 
     // Spawn the background worker
-    tokio::spawn(worker.run(cmd_rx, evt_tx));
+    tokio::spawn(player.run(cmd_rx, evt_tx));
 
     // Spawn receiver for worker events
     tokio::spawn(async move {
@@ -60,12 +106,14 @@ async fn main() -> Result<(), slint::PlatformError> {
     cmd_tx.send(PlayerCommand::Update("NewName".to_string())).unwrap();
 
 
-    let ui = MainWindow::new()?;
-    let ui_handle = ui.as_weak();
 
 
-    let audioPlayer = ui.global::<SlintAudioPlayer>();
-    audioPlayer.on_play({
+    let slint_app_window = MainWindow::new()?;
+    // let slint_app_window_weak = slint_app_window.as_weak();
+
+
+    let slint_audio_player = slint_app_window.global::<SlintAudioPlayer>();
+    slint_audio_player.on_play({
         let tx = cmd_tx.clone();
         move |file_name: SharedString| {
             tx.send(PlayerCommand::Update(file_name.to_string())).unwrap();
@@ -73,8 +121,8 @@ async fn main() -> Result<(), slint::PlatformError> {
     });
 
 
-    let slint_preferences = ui.global::<SlintPreferences>();
-    let preferences_ui = ui.clone_strong();
+    let slint_preferences = slint_app_window.global::<SlintPreferences>();
+    let preferences_ui = slint_app_window.clone_strong();
     slint_preferences.on_sync(move || {
         let pref = preferences_ui.global::<SlintPreferences>();
         
@@ -86,15 +134,15 @@ async fn main() -> Result<(), slint::PlatformError> {
             let mut file = OpenOptions::new()
                 .write(true) // <--------- this
                 .open(path).ok().unwrap();
-            write!(file, "{}", brightness_target_value);
+            let _ = write!(file, "{}", brightness_target_value);
         }
         println!("brightness: {}", brightness_target_value);
         
         println!("color-scheme: {}", pref.get_color_scheme());
     });
 
-    let navigation = ui.global::<SlintNavigation>();
-    let goto_ui = ui.clone_strong();
+    let navigation = slint_app_window.global::<SlintNavigation>();
+    let goto_ui = slint_app_window.clone_strong();
     navigation.on_goto(move |value| {
         let nav = goto_ui.global::<SlintNavigation>();
         nav.set_route(value);
@@ -117,7 +165,7 @@ async fn main() -> Result<(), slint::PlatformError> {
         nav.set_history_index(next_index);
     });
 
-    let back_ui = ui.clone_strong();
+    let back_ui = slint_app_window.clone_strong();
     navigation.on_back(move || {
         let nav = back_ui.global::<SlintNavigation>();
         let current_index = nav.get_history_index();
@@ -130,7 +178,7 @@ async fn main() -> Result<(), slint::PlatformError> {
         nav.set_history_index(current_index - 1);
     });
 
-    let forward_ui = ui.clone_strong();
+    let forward_ui = slint_app_window.clone_strong();
     navigation.on_forward(move || {
         let nav = forward_ui.global::<SlintNavigation>();
         let current_index = nav.get_history_index();
@@ -144,6 +192,6 @@ async fn main() -> Result<(), slint::PlatformError> {
     });
 
 
-    ui.run()
+    slint_app_window.run()
 }
 
--- src/player.rs
+++ src/player.rs
@@ -1,8 +1,7 @@
 // https://github.com/PaulWoitaschek/Voice/blob/main/core/playback/src/main/kotlin/voice/core/playback/player/VoicePlayer.kt
 // https://github.com/tsirysndr/music-player/blob/master/playback/src/audio_backend/rodio.rs
 
-use cpal::traits::{DeviceTrait, HostTrait};
-use rodio::{OutputStreamBuilder, Sink};
+use rodio::Sink;
 use std::fs::File;
 use std::path::Path;
 use std::time::Duration;
@@ -22,11 +21,15 @@ pub enum PlayerEvent {
 
 pub struct Player {
     pub name: String,
+    pub sink: Sink,
 }
 
 impl Player {
+    pub fn new( name: String, sink:Sink) -> Player {
+        Self { name, sink }
+    }
     pub async fn run(
-        mut self,
+        self,
         mut cmd_rx: mpsc::UnboundedReceiver<PlayerCommand>,
         evt_tx: mpsc::UnboundedSender<PlayerEvent>,
     ) {
@@ -55,64 +58,9 @@ impl Player {
     }
 
     async fn play(&self, p0: String) {
-        if(p0 != "preftest") {
+        if p0 != "preftest" {
             return;
         }
-
-        let host = cpal::default_host();
-        let mut device = None;
-        for _ in 1..3 {
-            device = Some(host.output_devices().unwrap()
-                .find(|d| {
-                    if let Ok(name) = d.name() {
-                        // sysdefault:CARD=A
-                        let match_string = "USB-C to 3.5mm Headphone Jack A";
-                        // ALSA device names may include "hw:2,0" or "hw-2-0"
-                        // You might need to tune this filter depending on your device naming
-                        println!("device: {} contains {}: {}", name, match_string, name.contains(match_string));
-                        name.contains(match_string)
-                    } else {
-                        false
-                    }
-                })).unwrap();
-
-
-            if device.is_some() {
-                break;
-            }
-        }
-
-        if !device.is_some() {
-            device = Some(host.default_output_device().unwrap());
-        }
-
-
-
-        let selected_device = device.unwrap();
-        /*
-        let default_config = selected_device.default_output_config().ok().unwrap();
-        let sample_rate = default_config.sample_rate().0;
-        let channel_count = default_config.channels();
-        let sample_format = default_config.sample_format();
-        */
-
-        let builder_result = OutputStreamBuilder::from_device(selected_device);
-        let builder = builder_result.unwrap();
-
-        // let builder = OutputStreamBuilder::from_default_device().unwrap();
-
-
-        let stream = builder.open_stream_or_fallback().unwrap();
-
-
-        let sink = Sink::connect_new(stream.mixer());
-        /*
-        let stream_handle = OutputStreamBuilder::open_default_stream().unwrap();
-        let sink = rodio::Sink::connect_new(stream_handle.mixer());
-
-        */
-
-
 
         let path_string = "/tmp/alert-work.ogg";
         let path_string_alternative = "/root/alert-work.ogg";
@@ -126,77 +74,7 @@ impl Player {
         }
 
         let file = File::open(path).unwrap();
-        sink.append(rodio::Decoder::try_from(file).unwrap());
-        sink.sleep_until_end();
+        self.sink.append(rodio::Decoder::try_from(file).unwrap());
+        self.sink.sleep_until_end();
     }
 }
-
-/*
-
-impl Player {
-    pub fn new() -> Self {
-        Player { name: "init".to_string() }
-    }
-
-    pub async fn play(&mut self, id: String) {
-        // Async play logic (load, decode, output audio)
-        println!("Playing file: {}", id);
-        if(id != "") {
-            /*
-            let open_result = awedio::sounds::open_file(id);
-            if(open_result.is_ok()) {
-                let (sound, notifier) = open_result.ok().unwrap()
-                    .pausable()
-                    .with_adjustable_volume()
-                    .with_adjustable_speed()
-                    .with_completion_notifier();
-                let sound = Box::new(sound);
-                // sound.set_paused(true);
-                // let sound_weak = sound.as_weak();
-
-                self.manager.play(sound);
-                let _ = notifier.recv();
-            }
-
-             */
-        }
-
-    }
-
-    pub async fn pause(&mut self) {
-        // self.manager.
-
-    }
-
-    // Implement other async controls: next, previous, fast_forward, rewind
-}
-
-*/
-
-/*
-Ideas:
-- PlayerState => Playing, Paused, Buffering, etc.
-- Events => TrackStarted, TrackEnded, PositionChanged, etc.
-- Metadata retrieval should not be part of the player
-- MediaItem => Reference to the Playable Tracks
-  - ItemMetadata: Metadata specific to the item (AlbumArtist, etc.)
-  - Tracks: List of tracks contained by this media item (e.g. when an audio book has multiple files)
-    - TrackMetadata: Metadata specific to the track (title, chapters, x of y, etc.)
-
- */
-
-
-/*
-    let Some(file_path) = args() else {
-        eprintln!("usage: FILE_PATH");
-        std::process::exit(2);
-    };
-
-    let (mut manager, _backend) = awedio::start()?;
-    let (sound, notifier) = awedio::sounds::open_file(file_path)?.with_completion_notifier();
-
-    manager.play(Box::new(sound));
-    let _ = notifier.recv();
-
-    Ok(())
- */
